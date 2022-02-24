434b7fea7cc2b152b99a98877f5558f93fbab58a
prateek <prateekkumar2811@gmail.com>
2022-02-24 13:48:51 +0530

    changes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <body>
     <div class="headerparent">
         <div id="header">
-            <a href="index.html"><img class="logo" src="files/images/logo.png" /></a>
+            <a href="https://testpetadopt.herokuapp.com/"><img class="logo" src="files/images/logo.png" /></a>
         </div>
         <div id="location">
             <form>
